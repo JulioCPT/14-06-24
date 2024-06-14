b68df4e8250bcefec333c4dc777c5e6ec62390c6
Júlio César Teixeira <130468355+JulioCPT@users.noreply.github.com>
2024-06-14 16:49:18 -0300

    Add files via upload

---
+++ criar_db_agencia_de_turismo.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS db_agencia_de_turismo;
+USE `db_agencia_de_turismo`;
---
+++ tbl_turistas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `tbl_turistas` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(255),
+    PRIMARY KEY(`id`)
+    );
+    
+    
